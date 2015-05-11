063ad55cfde6ea87f1b78629b0222e2286e8f62b
h4xxel <isaksson.axel@telia.com>
2015-05-11 19:49:18 +0200

    added log.h

--- kernel/printf.h
+++ kernel/printf.h
@@ -1,17 +1,6 @@
 #ifndef __PRINTF_H__
 #define	__PRINTF_H__
 
-typedef enum LogLevel {
-	LOG_LEVEL_NONE,
-	LOG_LEVEL_CRITICAL,
-	LOG_LEVEL_ERROR,
-	LOG_LEVEL_WARNING,
-	LOG_LEVEL_INFO,
-	LOG_LEVEL_DEBUG,
-	LOG_LEVELS
-}
-
-#define	kprintf(x,...)	printf(__VA_ARGS__)
 int printf(char *format, ...);
 
 #endif
