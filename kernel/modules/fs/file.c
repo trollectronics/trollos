f3ea5c89bf01245835298363bf9fdd9f4fe52a3b
Steven Arnow <s@rdw.se>
2017-02-24 23:09:23 +0100

    hm

--- kernel/modules/fs/file.c
+++ kernel/modules/fs/file.c
@@ -49,7 +49,7 @@ int fd_write(int uid, int fd, const void *buf, uint32_t count) {
 	int i, j, cnt;
 
 	for (j = 0; (j<<9) < count; j++) {
-		cnt = (count-(j<<9)>512?512:count);
+		cnt = (count-(j<<9)>512?512:(count-(j<<9));
 		memcpy_from_user(buff, buf, cnt);
 		terminal_write(0, fd, buff, cnt);
 	}
